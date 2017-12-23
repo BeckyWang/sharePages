fc09da509171a6b17116544675f6ec24e5e33150
BeckyWang <wangbeibei01018@163.com>
2017-12-23 13:52:37 +0800

    mpdify word_evaluation.js

--- js/word_evaluation.js
+++ js/word_evaluation.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	var clientHeight = document.documentElement.clientHeight;
+
 	var requestData = function() {
 		$.ajax({
 			url: 'http://test-phpadmin.seer-global.cn/api/user/share/wordAbility',
@@ -9,15 +11,32 @@ $(document).ready(function() {
 				if(result.code == 0) {
 					showData(result.data.statInfo);
 				} else {
-					alert(result.msg)
+					alert(result.msg);
 				}
 			}
 		});
 	};
 
 	var showData = function(data) {
+		$('.money').html('+' + data.coin);
+		$('.exp').html('+' + data.score);
+		$('.time').html(data.cost_time + 's');
+		$('.per').html(data.correct_ratio);
 
+		data.rank_list.forEach(function(val) {
+			var ulHtml = '<ul class="table-body-item">' +
+				'<li style="width: 15%;"' + (val.order < 4 && 'class="font-color"') + '>' + val.order + '</li>' +
+				'<li style="width: 20%;">' + val.username + '</li>' +
+				'<li style="width: 35%;">' + val.cost_time + '</li>' +
+				'<li style="width: 30%;">' + val.correct_ratio + '</li>' +
+			'</ul>';
+			$('.table-body').append(ulHtml);
+		});
 	};
 
-	requestData();
+	(function init() {
+		$('.table-body').css('height', (clientHeight - 245) + 'px');
+		$('.table-body').css('overflow', 'auto');
+		requestData();
+	})();
 });
